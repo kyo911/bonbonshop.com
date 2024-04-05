f922a15d02c0268a1aafd2bd1df4cc704c51ed4f
kyo911 <tdvinh912@gmail.com>
2024-04-05 20:58:43 +0700

    Thêm trang Giới thiệu

---
+++ gioi-thieu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giới thiệu</title>
+</head>
+<body>
+    <h1>Giới thiệu</h1>
+    <?php
+        echo 'Đây là trang <b style="col:red">Giới thiệu</b>'
+    ?>
+</body>
+</html>
