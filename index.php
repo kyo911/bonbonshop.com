2e5cbae1b4bc7a16034829e8d6c01814417a8424
kyo911 <tdvinh912@gmail.com>
2024-04-05 20:32:42 +0700

    Bổ sung trang chủ

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Xin chào PHP</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php
+            echo 'Xin chào PHP';
+        ?>
+
+    </body>
+</html>
